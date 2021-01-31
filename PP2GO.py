f0bf2e6b7f165e3ce8f0e6f5d2437c46d33bc0ac
Geovanny Risco <geovannyalexanderr@gmail.com>
2021-01-31 23:59:47 +0100

    Set skeleton for the pipeline

---
+++ PP2GO.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+__author__ = "Geovanny Risco"
+__email__ = "geovanny.risco@bsc.es"
+__version__ = "0.1"
+
+
+__DESCRIPTION__ = ""
+
+if __name__ == "__main___":
+    pass
